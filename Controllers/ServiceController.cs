34ccb3fcd4298e104d3aeebab5cec6d8c2bb9d99
Ezelhan Dikmenlioğlu <20060394@stu.omu.edu.tr>
2024-02-23 00:54:36 +0300

    ..

--- Controllers/ServiceController.cs
+++ Controllers/ServiceController.cs
@@ -3,15 +3,58 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using DevFolio.Models;
 
-namespace DevFolio.Controllers
+namespace DevFolio2.Controllers
 {
     public class ServiceController : Controller
     {
-        // GET: Service
+        DbDevFolioEntities6 db = new DbDevFolioEntities6();
         public ActionResult ServiceList()
         {
+            var values = db.TblService.ToList();
+            return View(values);
+        }
+
+        [HttpGet]
+        public ActionResult CreateService()
+        {
             return View();
+        }
+
+        [HttpPost]
+        public ActionResult CreateService(TblService s)
+        {
+            db.TblService.Add(s);
+            db.SaveChanges();
+            return RedirectToAction("ServiceList");
+        }
+
+        public ActionResult DeleteService(int id)
+        {
+            var value = db.TblService.Find(id);
+            db.TblService.Remove(value);
+            db.SaveChanges();
+            return RedirectToAction("ServiceList");
+        }
+
+        [HttpGet]
+        public ActionResult UpdateService(int id)
+        {
+            var value = db.TblService.Find(id);
+            return View(value);
+        }
+
+
+        [HttpPost]
+        public ActionResult UpdateService(TblService s)
+        {
+            var value = db.TblService.Find(s.ServiceID);
+            value.ServiceTitle = s.ServiceTitle;
+            value.Desciption = s.Desciption;
+            value.ServiceImageUrl = s.ServiceImageUrl;
+            db.SaveChanges();
+            return RedirectToAction("ServiceList");
         }
     }
 }
